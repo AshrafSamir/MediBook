c013f7b8353c75d79e60eb9a95ba0de628bb86a1
Mahmouud74 <mahmouudmoohamed@gmail.com>
2023-04-16 00:22:50 +0200

    addModels

--- models/doctorTimeSlots.model.js
+++ models/doctorTimeSlots.model.js
@@ -2,13 +2,13 @@ const mongoose=require('mongoose');
 
  const doctoTimeSlots=mongoose.Schema({
     doctorId:{type:mongoose.Schema.Types.ObjectId,ref:'user', require:true},
-    schedule:[{
+    timeSlot:{
         from:{type:Date},
         to:{type:Date},
         day:{type:Date},
         maxReservations:{type:Number},
         fullyBooked:{type:Boolean,default:false}
-    }]
+    }
 });
 module.exports = mongoose.model('doctorTimeSlot', doctoTimeSlots);
 
