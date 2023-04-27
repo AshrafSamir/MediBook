af4c5dd52b099111abfbd93fb56ef59ebe37ef60
Mahmouud74 <mahmouudmoohamed@gmail.com>
2023-04-27 20:12:07 +0200

    add get Time Slots

--- routes/doctorSchedule.routes.js
+++ routes/doctorSchedule.routes.js
@@ -5,10 +5,13 @@ const {
   doctorTimeSlots,
   getDoctorsInfo,
   getTimeSlotBookings,
+  timeSlotById
 } = require("../controllers/doctorSchedule.controller");
 
 doctorScheduleRoute.post("/createtimeslots/:id", auth, createSchedule);
 doctorScheduleRoute.get("/doctorTimeSlots/:id", doctorTimeSlots);
 doctorScheduleRoute.get("/getDoctorsInfo", getDoctorsInfo);
 doctorScheduleRoute.get("/gettimeSlotbookings/:doctorId/:timeSlotId", getTimeSlotBookings);
+doctorScheduleRoute.get("/gettimeslot/:id", timeSlotById);
+
 module.exports = doctorScheduleRoute;
